6140f5c1b7325999e659e3285ffd8425497b4b86
Ummulkhair <ummulkahir@gmail.com>
2023-03-16 16:53:50 -0700

    hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function of my project
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
