ab786cba9348e080e1aedc8feeb31ec7522f5b4a
Ummulkhair <ummulkahir@gmail.com>
2023-04-11 09:51:43 -0700

    first static libraries commit

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+this directory contains files on static libraries project
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'prints out a quote'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{_puts(
+		"\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
+}
